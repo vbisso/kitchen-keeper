bbab3ae82dd7d37c009130b99c46e2a96e929847
vbisso <valeriabisso19@gmail.com>
2025-11-28 15:10:36 -0700

    render url

--- kitchen-keeper-frontend/app.config.js
+++ kitchen-keeper-frontend/app.config.js
@@ -7,6 +7,7 @@ export default {
     version: "1.0.0",
     extra: {
       OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+      apiBaseUrl: process.env.API_BASE_URL,
     },
   },
 };
--- kitchen-keeper-frontend/utils/config.js
+++ kitchen-keeper-frontend/utils/config.js
@@ -1,14 +1,18 @@
+// import Constants from "expo-constants";
+// import { Platform } from "react-native";
+
+// const LAN_IP = "192.168.0.16";
+// // const LAN_IP = "10.244.190.231"; //BYUI WIFI
+
+// const isDevice = Constants.appOwnership === "expo"; // true on Expo Go
+// const isSimulator =
+//   (Platform.OS === "ios" && !isDevice) ||
+//   (Platform.OS === "android" && !isDevice);
+
+// export const API_BASE_URL = isSimulator
+//   ? "http://localhost:3000"
+//   : `http://${LAN_IP}:3000`;
+
 import Constants from "expo-constants";
-import { Platform } from "react-native";
 
-const LAN_IP = "192.168.0.16";
-// const LAN_IP = "10.244.190.231"; //BYUI WIFI
-
-const isDevice = Constants.appOwnership === "expo"; // true on Expo Go
-const isSimulator =
-  (Platform.OS === "ios" && !isDevice) ||
-  (Platform.OS === "android" && !isDevice);
-
-export const API_BASE_URL = isSimulator
-  ? "http://localhost:3000"
-  : `http://${LAN_IP}:3000`;
+export const API_BASE_URL = Constants.expoConfig.extra.apiBaseUrl;
